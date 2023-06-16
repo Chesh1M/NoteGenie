464859b7aeec50087814375b03d91e320eb2fd82
Chesh1M <aowenc@gmail.com>
2023-06-16 11:43:58 +0800

    Added validation for registration page fields and onfocus listeners for the fields to identify if the fields are incorrectly filled.

--- app/src/main/java/com/example/notegenie/RegisterActivity.kt
+++ app/src/main/java/com/example/notegenie/RegisterActivity.kt
@@ -2,10 +2,176 @@ package com.example.notegenie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import com.example.notegenie.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener, View.OnFocusChangeListener, View.OnKeyListener {
+
+    private lateinit var mBinding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        mBinding = ActivityRegisterBinding.inflate(LayoutInflater.from(this))
+        setContentView(mBinding.root)
+        mBinding.fullNameEt.onFocusChangeListener = this
+        mBinding.emailEt.onFocusChangeListener = this
+        mBinding.passwordEt.onFocusChangeListener = this
+        mBinding.cPasswordEt.onFocusChangeListener = this
+    }
+
+    /* REGISTRATION FIELDS VALIDATION */
+    // Validate full name
+    private fun validateFullName(): Boolean{
+        var errorMessage: String? = null
+        val value: String = mBinding.fullNameEt.text.toString()
+        if (value.isEmpty()){
+            errorMessage = "Full name is required"
+        }
+
+        // if validation fails
+        if (errorMessage != null){
+            mBinding.fullNameTil.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+        return errorMessage == null
+    }
+
+    // Validate email
+    private fun validateEmail(): Boolean {
+        var errorMessage: String? = null
+        val value = mBinding.emailEt.text.toString()
+        if (value.isEmpty()){
+            errorMessage  = "Email is required"
+        }
+        else if (Patterns.EMAIL_ADDRESS.matcher(value).matches()) { // if email entered is a valid email address
+            errorMessage = "Email address is invalid"
+        }
+
+        // if validation fails
+        if (errorMessage != null){
+            mBinding.emailTil.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+        return errorMessage == null
+    }
+
+    // Validate password
+    private fun validatePassword(): Boolean {
+        var errorMessage: String? = null
+        val value = mBinding.passwordEt.text.toString()
+        if (value.isEmpty()){
+            errorMessage  = "Password is required"
+        }
+        else if (value.length < 6) { // if email entered is a valid email address
+            errorMessage = "Password must be at least 6 characters long"
+        }
+
+        // if validation fails
+        if (errorMessage != null){
+            mBinding.passwordTil.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+        return errorMessage == null
+    }
+
+    // Validate confirm password
+    private fun validateConfirmPassword(): Boolean {
+        var errorMessage: String? = null
+        val value = mBinding.cPasswordEt.text.toString()
+        if (value.isEmpty()){
+            errorMessage  = "Confirm Password is required"
+        }
+        else if (value.length < 6) { // if email entered is a valid email address
+            errorMessage = "Confirm Password must be at least 6 characters long"
+        }
+
+        // if validation fails
+        if (errorMessage != null){
+            mBinding.cPasswordTil.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+        return errorMessage == null
+    }
+
+    // Validate password and confirm password
+    private fun validatePasswordAndConfirmPassword(): Boolean {
+        var errorMessage: String? = null
+        val password = mBinding.passwordEt.text.toString()
+        val confirmPassword = mBinding.cPasswordEt.text.toString()
+        if (password != confirmPassword){
+            errorMessage = "Confirm password and password do not match!"
+        }
+
+        // if validation fails
+        if (errorMessage != null){
+            mBinding.cPasswordTil.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+        return errorMessage == null
+    }
+    /* // REGISTRATION FIELDS VALIDATION */
+
+    override fun onClick(view: View?) {
+        TODO("Haven't done")
+    }
+
+    override fun onFocusChange(view: View?, hasFocus: Boolean) {
+        if (view != null) {
+            when(view.id) {
+                R.id.fullNameEt -> {
+                    if (hasFocus) {
+                        if (mBinding.fullNameTil.isErrorEnabled){
+                            mBinding.fullNameTil.isErrorEnabled = false
+                        }
+                    } else {
+                        validateFullName()
+                    }
+                }
+                R.id.emailEt -> {
+                    if (hasFocus) {
+                        if (mBinding.emailTil.isErrorEnabled){
+                            mBinding.emailTil.isErrorEnabled = false
+                        }
+                    } else {
+                        validateEmail()
+                    }
+                }
+                R.id.passwordEt -> {
+                    if (hasFocus) {
+                        if (mBinding.passwordTil.isErrorEnabled){
+                            mBinding.passwordTil.isErrorEnabled = false
+                        }
+                    } else {
+                        validatePassword()
+                    }
+                }
+                R.id.cPasswordEt -> {
+                    if (hasFocus) {
+                        if (mBinding.cPasswordTil.isErrorEnabled){
+                            mBinding.cPasswordTil.isErrorEnabled = false
+                        }
+                    } else {
+                        validateConfirmPassword()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onKey(view: View?, event: Int, keyEvent: KeyEvent?): Boolean {
+        return false
     }
 }
