c68fcf689f20d67a32a303b854ca2c214e363bce
Aditya Sivadas <134035198+ADYMRL23@users.noreply.github.com>
2023-06-19 16:16:34 +0800

    Revert "Basically just the summary page"
    This reverts commit 78b080ad4d52c75d8ca1ffc1ea9c89882f00d4fa.

--- app/src/main/java/com/example/notegenie/MainActivity.kt
+++ app/src/main/java/com/example/notegenie/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.notegenie
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,9 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Changing intent to another page
-        val goToSummaryPage = Intent(this, SummaryPage::class.java)
-        startActivity(goToSummaryPage)
     }
 }
