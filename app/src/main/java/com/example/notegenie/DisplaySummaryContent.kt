aa076cdc33c20fdd1b93aa544334ad0837408433
Aditya Sivadas <134035198+ADYMRL23@users.noreply.github.com>
2023-06-20 14:58:43 +0800

    Added the translation frature
    Added the translation feature to the SummaryContent page

--- app/src/main/java/com/example/notegenie/DisplaySummaryContent.kt
+++ app/src/main/java/com/example/notegenie/DisplaySummaryContent.kt
@@ -3,6 +3,7 @@ package com.example.notegenie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
@@ -12,6 +13,10 @@ import android.widget.TextView
 import android.widget.Toast
 import com.example.notegenie.databinding.ActivityDisplaySummaryContentBinding
 import com.example.notegenie.databinding.ActivitySummaryCardBinding
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 
 class DisplaySummaryContent : AppCompatActivity() {
 
@@ -45,7 +50,6 @@ class DisplaySummaryContent : AppCompatActivity() {
         // Setting the text into the view
         currentLanguageTextView.text = CURRENT_LANGUAGE
 
-        // Initializing a translation Object
 
         // Initializing the pop-up menu
 
@@ -89,6 +93,39 @@ class DisplaySummaryContent : AppCompatActivity() {
                 Toast.makeText(this, "Language Changed to Chinese",
                     Toast.LENGTH_SHORT).show()
 
+                // Create an English-Chinese translator:
+                val options = TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.ENGLISH)
+                    .setTargetLanguage(TranslateLanguage.CHINESE)
+                    .build()
+                val englishChineseTranslator = Translation.getClient(options)
+
+                // Making sure that the model is available
+                val conditions = DownloadConditions.Builder()
+                    .requireWifi()
+                    .build()
+                englishChineseTranslator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener {
+                        Log.i("Translation Load Status", "Sucessfully Translated")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.i("Translation Load Status", "Translation Model Load Failed")
+                        // Showing that the translation failed
+                        Toast.makeText(this, "Translation Model Load Failed",
+                            Toast.LENGTH_LONG).show()
+                    }
+
+                // Actually translating
+                englishChineseTranslator.translate(binding.summaryContentsView.text.toString())
+                    .addOnSuccessListener { translatedText ->
+                        binding.summaryContentsView.text = translatedText.toString()
+                        Log.i("Translation Status:", "Sucessfuly Translated")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.i("Translation Status:", "Error Translating")
+                    }
+
+
             } else if(menuID==2){
 
                 // Switching the Language
@@ -98,6 +135,39 @@ class DisplaySummaryContent : AppCompatActivity() {
                 // Notifying the user of the change
                 Toast.makeText(this, "Language Changed to Tamil",
                     Toast.LENGTH_SHORT).show()
+
+                // Create an English-Tamil translator:
+                val options = TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.ENGLISH)
+                    .setTargetLanguage(TranslateLanguage.TAMIL)
+                    .build()
+                val englishTamilTranslator = Translation.getClient(options)
+
+                // Making sure that the model is available
+                val conditions = DownloadConditions.Builder()
+                    .requireWifi()
+                    .build()
+                englishTamilTranslator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener {
+                        Log.i("Translation Load Status", "Sucessfully Translated")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.i("Translation Load Status", "Translation Model Load Failed")
+                        // Showing that the translation failed
+                        Toast.makeText(this, "Translation Model Load Failed",
+                            Toast.LENGTH_LONG).show()
+                    }
+
+                // Actually translating
+                englishTamilTranslator.translate(binding.summaryContentsView.text.toString())
+                    .addOnSuccessListener { translatedText ->
+                        binding.summaryContentsView.text = translatedText.toString()
+                        Log.i("Translation Status:", "Sucessfuly Translated")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.i("Translation Status:", "Error Translating")
+                    }
+
             } else if(menuID==3){
 
                 // Switching the Language
@@ -107,9 +177,43 @@ class DisplaySummaryContent : AppCompatActivity() {
                 // Notifying the user of the change
                 Toast.makeText(this, "Language Changed to Spanish",
                     Toast.LENGTH_SHORT).show()
+
+                // Create an English-Tamil translator:
+                val options = TranslatorOptions.Builder()
+                    .setSourceLanguage(TranslateLanguage.ENGLISH)
+                    .setTargetLanguage(TranslateLanguage.SPANISH)
+                    .build()
+                val englishSpanishTranslator = Translation.getClient(options)
+
+                // Making sure that the model is available
+                val conditions = DownloadConditions.Builder()
+                    .requireWifi()
+                    .build()
+                englishSpanishTranslator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener {
+                        Log.i("Translation Load Status", "Sucessfully Translated")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.i("Translation Load Status", "Translation Model Load Failed")
+                        // Showing that the translation failed
+                        Toast.makeText(this, "Translation Model Load Failed",
+                            Toast.LENGTH_LONG).show()
+                    }
+
+                // Actually translating
+                englishSpanishTranslator.translate(binding.summaryContentsView.text.toString())
+                    .addOnSuccessListener { translatedText ->
+                        binding.summaryContentsView.text = translatedText.toString()
+                        Log.i("Translation Status:", "Sucessfuly Translated")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.i("Translation Status:", "Error Translating")
+                    }
             }
 
             false
+
+
 
         }
 
