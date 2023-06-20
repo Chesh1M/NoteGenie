429c30d903d8f3fadd49c4153aabfb94780721f1
Aditya Sivadas <134035198+ADYMRL23@users.noreply.github.com>
2023-06-20 14:04:00 +0800

    Added the pop-up view for the SummaryContent page

--- app/src/main/java/com/example/notegenie/DisplaySummaryContent.kt
+++ app/src/main/java/com/example/notegenie/DisplaySummaryContent.kt
@@ -1,12 +1,22 @@
 package com.example.notegenie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import android.widget.Toast
 import com.example.notegenie.databinding.ActivityDisplaySummaryContentBinding
 import com.example.notegenie.databinding.ActivitySummaryCardBinding
 
 class DisplaySummaryContent : AppCompatActivity() {
+
+    // Initializing Global Variables
+    var CURRENT_LANGUAGE = "En"
 
     // Initializing a binding
     private lateinit var binding: ActivityDisplaySummaryContentBinding
@@ -27,6 +37,90 @@ class DisplaySummaryContent : AppCompatActivity() {
         // Setting up the text views accordingly
         binding.summaryTextView.text = summaryTitle
         binding.summaryContentsView.text = summaryContent
+
+        // Setting the value of the currentLanguageTextView onCreate
+
+        // Initializing the view
+        val currentLanguageTextView: TextView = findViewById(R.id.currentLanguageTextView)
+        // Setting the text into the view
+        currentLanguageTextView.text = CURRENT_LANGUAGE
+
+        // Initializing a translation Object
+
+        // Initializing the pop-up menu
+
+        // Firstly initializing the widget
+        val languagesMenuView: ImageView = findViewById(R.id.changeLanguageImageView)
+
+        // Now initializing the pop-up menu
+        val changeLanguagePopupMenu = PopupMenu(this, languagesMenuView)
+
+        // Adding the elements of the popup menu
+        changeLanguagePopupMenu.menu.add(Menu.NONE, 0, 0, "English (En)")
+        changeLanguagePopupMenu.menu.add(Menu.NONE, 1, 1, "Chinese (Ch)")
+        changeLanguagePopupMenu.menu.add(Menu.NONE, 2, 2, "Tamil (Ta)")
+        changeLanguagePopupMenu.menu.add(Menu.NONE, 3, 3, "Spanish (Sp)")
+
+        // Handling item clicks
+        changeLanguagePopupMenu.setOnMenuItemClickListener { menuItem->
+
+            // Getting id of menu
+            val menuID = menuItem.itemId
+
+
+            // Handling navigation
+            if (menuID==0){
+
+                // Switching the Language
+                CURRENT_LANGUAGE = "En"
+                currentLanguageTextView.text = CURRENT_LANGUAGE
+
+                // Notifying the user of the change
+                Toast.makeText(this, "Language Changed to English",
+                    Toast.LENGTH_SHORT).show()
+
+            } else if(menuID==1){
+
+                // Switching the Language
+                CURRENT_LANGUAGE = "Ch"
+                currentLanguageTextView.text = CURRENT_LANGUAGE
+
+                // Notifying the user of the change
+                Toast.makeText(this, "Language Changed to Chinese",
+                    Toast.LENGTH_SHORT).show()
+
+            } else if(menuID==2){
+
+                // Switching the Language
+                CURRENT_LANGUAGE = "Ta"
+                currentLanguageTextView.text = CURRENT_LANGUAGE
+
+                // Notifying the user of the change
+                Toast.makeText(this, "Language Changed to Tamil",
+                    Toast.LENGTH_SHORT).show()
+            } else if(menuID==3){
+
+                // Switching the Language
+                CURRENT_LANGUAGE = "Sp"
+                currentLanguageTextView.text = CURRENT_LANGUAGE
+
+                // Notifying the user of the change
+                Toast.makeText(this, "Language Changed to Spanish",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            false
+
+        }
+
+        // Setting an onclick listener for the changeLanguageImageView
+        languagesMenuView.setOnClickListener{
+            changeLanguagePopupMenu.show()
+        }
+
+
+
+
 
     }
 }
